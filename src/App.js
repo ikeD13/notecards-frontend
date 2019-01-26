eee7b038c186730cbc9987586a4bbdf9db32647d
Galvanize Web Development <Galvanize@Isaacs-MacBook-Pro.local>
2019-01-26 16:36:58 -0700

    added complete functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './Cards/card.js'
+import DrawButton from './Button/button.js';
 
-class App extends Component {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+      currentCard: {}
+    }
+
+    this.updateCard = this.updateCard.bind(this);
+  }
+
+  async componentDidMount() {
+    const currentCards = this.state.cards.slice(0)
+   let data = await fetch('http://localhost:3000',{mode:"cors"})
+      .then(function(response) {
+        return response.json();
+      })
+      .catch(function(err) {
+        console.log(err);
+     });
+      console.log(data)
+      data.forEach(function({id, name, description}) {
+        description = description.replace(/"/g,"");
+        currentCards.push({id, name, description});
+      });
+      
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    }, ()=>{
+      console.log(this.state)
+    });
+  }
+
+  getRandomCard(currentCards) {
+    let card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return card;
+  }
+
+  updateCard() {
+    const currentCards = this.state.cards;
+    this.setState({
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+      <header>Flash Fo Yo Azz</header>
+      <h1>Lets Get Learnt!</h1>
+        <div className='card-row'> 
+          <Card question={this.state.currentCard.name}
+                answer={this.state.currentCard.id}
+                description={this.state.currentCard.description} 
+          />
+        </div>
+        <div className='button-row'>
+          <DrawButton drawCard={this.updateCard} />
+        </div>
       </div>
     );
   }
 }
-
-export default App;
